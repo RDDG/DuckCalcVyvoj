29cb49577dad795bd840f8bde10594645322d8bb
pavel <p.dohnalik@centrum.cz>
2016-04-10 02:15:06 +0200

    opravena vetev grafikz

---
+++ DuckCalcGUI/src/grafika/grafika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grafika;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+/**
+ *
+ * @author pavel
+ */
+public class grafika extends JFrame{
+    
+    public grafika(){
+        super("kalkulacka");
+        setSize(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        vykresli();
+    }
+    
+    private void vykresli(){
+    
+    }
+}
